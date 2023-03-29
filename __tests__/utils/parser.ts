a5a0c0f111d8d2f58b6592c2b1e51c18f3765242
Onur Ozdemir <33833917+oozd@users.noreply.github.com>
2023-03-29 12:04:30 +0300

    revert/single or two digit (#41)
    * revert single or two digit

--- __tests__/utils/parser.ts
+++ __tests__/utils/parser.ts
@@ -2,18 +2,10 @@ import { endOfDay, startOfDay } from "../../src/date_utils"
 
 export function parseStartDate(startDate: string): Date {
     const [year, month, day] = startDate.split('-')
-    const date = new Date()
-    date.setFullYear(parseInt(year))
-    date.setMonth(parseInt(month) - 1)
-    date.setDate(parseInt(day))
-    return startOfDay(date)
+    return startOfDay(new Date(parseInt(year), parseInt(month) - 1, parseInt(day)))
 }
 
 export function parseEndDate(endDate: string): Date {
     const [year, month, day] = endDate.split('-')
-    const date = new Date()
-    date.setFullYear(parseInt(year))
-    date.setMonth(parseInt(month) - 1)
-    date.setDate(parseInt(day))
-    return endOfDay(date)
+    return endOfDay(new Date(parseInt(year), parseInt(month) - 1, parseInt(day)))
 }
